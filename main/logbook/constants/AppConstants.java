6fe5af2382e653705fafa5f0607e92246e73208f
Ibemu <aa2a3a4a5a6a7a8a9@yahoo.co.jp>
2015-07-18 21:43:38 +0900

    リリース: 0.9+1.1.0

--- main/logbook/constants/AppConstants.java
+++ main/logbook/constants/AppConstants.java
@@ -26,7 +26,7 @@ public class AppConstants {
     public static final String NAME_PLUS = "航海日誌+";
 
     /** バージョン */
-    public static final Version VERSION_PLUS = new Version(1, 0, 6);
+    public static final Version VERSION_PLUS = new Version(1, 1, 0);
 
     /** バージョン */
     public static final String VERSION_FULL = VERSION + "+" + VERSION_PLUS.toStringFull();
